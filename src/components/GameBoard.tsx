c628298ff6b4131ec1aadec55401bb3e35bc3854
Maja Panoska <panoskamaja@outlook.com>
2026-07-22 22:44:07 +0200

    Created initial game board layout

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,55 @@
+export default function GameBoard(){
+    return (
+        <div className="flex flex-col gap-6">
+            <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400">A</div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        <div className="flex gap-1">
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+            <div className="tile w-12 h-12 border border-gray-400"></div>
+        </div>
+        
+        </div>
+        
+    )
+}
