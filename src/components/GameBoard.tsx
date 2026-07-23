ce8c40c8a6baa3183d874cb65b92de969cedf837
Maja Panoska <panoskamaja@outlook.com>
2026-07-23 22:38:44 +0200

    Implemented board state and added tile navigation

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,55 +1,63 @@
-export default function GameBoard(){
-    return (
-        <div className="flex flex-col gap-6">
-            <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400">A</div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        <div className="flex gap-1">
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-            <div className="tile w-12 h-12 border border-gray-400"></div>
-        </div>
-        
-        </div>
-        
-    )
+"use client";
+import { useState } from "react";
+
+export default function GameBoard() {
+  const [board, setBoard] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  const [currentColumn, setCurrentColumn] = useState(0);
+  const [currentRow, setCurrentRow] = useState(0);
+
+  //0 based index
+  const maxNumColumns = 4; //5 columns
+  const maxNumRows = 5; //6 rows
+
+  function updatePosition(){
+    if(currentRow === maxNumRows && currentColumn === maxNumColumns){
+        return;
+    }
+
+    if(currentColumn != maxNumColumns){
+        setCurrentColumn(prev => prev + 1);
+    }
+    else{
+        setCurrentColumn(0);
+        setCurrentRow(prev => prev + 1)
+    }
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          const newBoard = [...board];
+
+          newBoard[currentRow][currentColumn] = "А";
+
+          updatePosition();
+
+          setBoard(newBoard);
+        }}
+      >
+        Add A
+      </button>
+      <div className="flex flex-col gap-1.5">
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex gap-1.5">
+            {row.map((tile, tileIndex) => (
+              <div key={tileIndex} className="w-12 h-12 border border-gray-400">
+                {tile}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
