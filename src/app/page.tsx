e4b48da8f40290d6a4e2e0d5595413c38182c809
Maja Panoska <panoskamaja@outlook.com>
2026-07-30 20:05:19 +0200

    Fixed remove letter on last position issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,154 +1,18 @@
 "use client";
-import { useEffect, useState } from "react";
 import GameBoard from "@/components/GameBoard";
 import Keyboard from "@/components/Keyboard";
-import { checkWord } from "@/utils/wordChecker";
+import { useZborcheGame } from "@/hooks/useZborcheGame";
 
 export default function Home() {
-  const [board, setBoard] = useState([
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-  ]);
-
-  const [colors, setColors] = useState([
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-  ]);
-
-  const [currentColumn, setCurrentColumn] = useState(0);
-  const [currentRow, setCurrentRow] = useState(0);
-
-  //0 based index
-  const maxNumColumns = 4; //5 columns
-  const maxNumRows = 5; //6 rows
-
   const secretWord = "КУЌАА";
 
-  const [submittedWords, setSubmittedWords] = useState([]);
-  const [submittedRows, setSubmittedRows] = useState([]);
-
-  const [gameStatus, setGameStatus] = useState("PLAYING");
-
-  function updatePosition() {
-    if (currentRow === maxNumRows && currentColumn === maxNumColumns) {
-      return;
-    }
-
-    if (currentColumn != maxNumColumns) {
-      setCurrentColumn((prev) => prev + 1);
-    }
-    // else {
-    //   setCurrentColumn(0);
-    //   setCurrentRow((prev) => prev + 1);
-    // }
-  }
-
-  function addLetter(letter) {
-    const newBoard = [...board];
-
-    newBoard[currentRow][currentColumn] = letter;
-
-    updatePosition();
-
-    setBoard(newBoard);
-  }
-
-  function removeLetter() {
-    const newBoard = [...board];
-
-    let newColumn = currentColumn;
-
-    if (currentColumn === 0) {
-      return;
-    } else {
-      newColumn = currentColumn - 1;
-    }
-
-    newBoard[currentRow][newColumn] = "";
-
-    setBoard(newBoard);
-    setCurrentColumn(newColumn);
-  }
-
-  function submitWord() {
-    const hasEmptyTile = board[currentRow].some((letter) => letter === "");
-
-    if (hasEmptyTile) {
-      console.log("Need 5 letters");
-      return;
-    }
-
-    const currentWord = board[currentRow].join("");
-    addSubmittedWords(currentWord);
-    setSubmittedRows((prev) => [...prev, currentRow]);
-    if (currentWord === secretWord) {
-      console.log("ПОБЕДА");
-      setGameStatus("WON");
-    } else {
-      if (currentRow === maxNumRows) {
-        setGameStatus("LOST");
-      } else {
-        console.log("ГРЕШЕН ЗБОР. ПРОДОЛЖИ!");
-      }
-    }
-
-    const resultColors = checkWord(currentWord, secretWord);
-
-    const newColors = colors.map((row) => {
-      return [...row];
-    });
-
-    newColors[currentRow] = resultColors;
-
-    setColors(newColors);
-
-    if (currentWord != secretWord) {
-      setCurrentRow((prev) => prev + 1);
-      setCurrentColumn(0);
-    }
-  }
-
-  function handleKeyPress(letter) {
-    if (gameStatus !== "PLAYING") {
-      return;
-    }
-    console.log(letter);
-    if (letter === "⌫") {
-      removeLetter();
-    } else if (letter === "ENTER") {
-      submitWord();
-    } else {
-      addLetter(letter);
-    }
-  }
-
-  function addSubmittedWords(word) {
-    setSubmittedWords((prev) => [...prev, word]);
-  }
-
-  useEffect(() => {
-    console.log(submittedWords);
-  }, [submittedWords]);
-
-  useEffect(() => {
-    console.log(submittedRows);
-  }, [submittedRows]);
-
-  useEffect(() => {
-    console.log(colors);
-  }, [colors]);
-
-  useEffect(() => {
-    console.log(gameStatus);
-  }, [gameStatus]);
+   const {
+    board,
+    colors,
+    currentRow,
+    currentColumn,
+    handleKeyPress
+  } = useZborcheGame(secretWord);
 
   return (
     <main>
---
+++ src/hooks/useZborcheGame.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { checkWord } from "@/utils/wordChecker";
+
+export function useZborcheGame(secretWord) {
+  const [board, setBoard] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  const [colors, setColors] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  const [currentColumn, setCurrentColumn] = useState(0);
+  const [currentRow, setCurrentRow] = useState(0);
+
+  const [gameStatus, setGameStatus] = useState("PLAYING");
+
+  const WORD_LENGTH = 5;
+  const MAX_ATTEMPTS = 6;
+
+  const LAST_LETTER_INDEX = WORD_LENGTH - 1;
+  const LAST_ROW_INDEX = MAX_ATTEMPTS - 1;
+
+  function updatePosition() {
+    setCurrentColumn((prev) => Math.min(prev + 1, WORD_LENGTH));
+  }
+
+  function addLetter(letter) {
+    if (currentColumn > LAST_LETTER_INDEX) {
+      return;
+    }
+    const newBoard = board.map((row) => [...row]);
+
+    newBoard[currentRow][currentColumn] = letter;
+
+    updatePosition();
+
+    setBoard(newBoard);
+  }
+
+  function removeLetter() {
+    const newBoard = board.map((row) => [...row]);
+
+    let newColumn = currentColumn;
+
+    if (currentColumn === 0) {
+      return;
+    } else {
+      newColumn = currentColumn - 1;
+    }
+
+    newBoard[currentRow][newColumn] = "";
+
+    setBoard(newBoard);
+    setCurrentColumn(newColumn);
+  }
+
+  function submitWord() {
+    const hasEmptyTile = board[currentRow].some((letter) => letter === "");
+
+    if (hasEmptyTile) {
+      return;
+    }
+
+    const currentWord = board[currentRow].join("");
+    if (currentWord === secretWord) {
+      setGameStatus("WON");
+    } else {
+      if (currentRow === LAST_ROW_INDEX) {
+        setGameStatus("LOST");
+      } else {
+      }
+    }
+
+    const resultColors = checkWord(currentWord, secretWord);
+
+    const newColors = colors.map((row) => {
+      return [...row];
+    });
+
+    newColors[currentRow] = resultColors;
+
+    setColors(newColors);
+
+    if (currentWord != secretWord) {
+      setCurrentRow((prev) => prev + 1);
+      setCurrentColumn(0);
+    }
+  }
+
+  function handleKeyPress(letter) {
+    if (gameStatus !== "PLAYING") {
+      return;
+    }
+    if (letter === "⌫") {
+      removeLetter();
+    } else if (letter === "ENTER") {
+      submitWord();
+    } else {
+      addLetter(letter);
+    }
+  }
+
+  return {
+    board,
+    colors,
+    currentRow,
+    currentColumn,
+    gameStatus,
+    handleKeyPress,
+  };
+}
